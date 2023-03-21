79eb1d0bc0936b00fd74a4bb17a95843529af4e1
emasru <40176292+emasru@users.noreply.github.com>
2023-03-21 19:38:14 +0100

    Added GUI and check for hands

--- src/main/java/no/ntnu/idatt2001/oblig3/cardgame/Main.java
+++ src/main/java/no/ntnu/idatt2001/oblig3/cardgame/Main.java
@@ -8,16 +8,17 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.io.File;
 import javafx.scene.text.Text;
 import javafx.scene.text.Font;
 import javafx.scene.paint.Color;
+import javafx.geometry.Insets;
 
 
 import java.util.ArrayList;
@@ -28,42 +29,149 @@ public class Main extends Application {
         launch(args);
     }
 
-    /*
-    Using javaFX, make a window. The window contains a window within that shows a hand of playing cards.
-    On the right side there are two buttons: "Deal hand" and "Check hand".
-    At the bottom: four display boxes showing text values, with a description inline of what the value represents
-     */
+    public static HBox createInfoField(String text){
+        HBox textBox = new HBox();
+        Text t = new Text(text);
+        t.setFont(new Font(12));
+        TextField textField = new TextField();
+        textField.setDisable(true);
+        textBox.getChildren().add(t);
+        textBox.getChildren().add(textField);
+        textBox.setPadding(new Insets(5));
+
+        return textBox;
+    }
+
+    public static void showSumOfFaces(ArrayList<PlayingCard> hand, FlowPane pane){
+        HBox sumBox = (HBox) pane.getChildren().get(0);
+        TextField sumField = (TextField) sumBox.getChildren().get(1);
+
+        int sum = 0;
+
+        for (PlayingCard card : hand) {
+            sum += card.getFace();
+        }
+
+        sumField.setText(String.valueOf(sum));
+    }
+
+    public static void showCardsOfHearts(ArrayList<PlayingCard> hand, FlowPane pane){
+        HBox heartBox = (HBox) pane.getChildren().get(1);
+        TextField heartField = (TextField) heartBox.getChildren().get(1);
+
+        String hearts = "";
+
+        for (PlayingCard card : hand) {
+            if (card.getSuit() == 'H') {
+                hearts = hearts.concat(card.getAsString() + " ");
+            }
+        }
+
+        if (hearts.equals("")) {
+            hearts = "No hearts";
+        }
+
+        heartField.setText(hearts);
+    }
+
+    public static void showFlush(ArrayList<PlayingCard> hand, FlowPane pane){
+        HBox flushBox = (HBox) pane.getChildren().get(2);
+        TextField flushField = (TextField) flushBox.getChildren().get(1);
+
+        flushField.setText("Yes");
+
+        // Get the first card's suit, and check if the rest of the cards have the same suit
+        char startSuit = hand.get(0).getSuit();
+        for (PlayingCard card : hand) {
+            if (card.getSuit() != startSuit) {
+                flushField.setText("No");
+                return;
+            }
+        }
+    }
+
+    public static void showQueenOfSpades(ArrayList<PlayingCard> hand, FlowPane pane){
+        HBox queenBox = (HBox) pane.getChildren().get(3);
+        TextField queenField = (TextField) queenBox.getChildren().get(1);
+
+        queenField.setText("No");
+        for (PlayingCard card : hand) {
+            if (card.getSuit() == 'S' && card.getFace() == 12) {
+                queenField.setText("Yes");
+                return;
+            }
+        }
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Card Game");
-        Button button = new Button();
-        button.setText("Click me");
+        HBox cardBox = new HBox();
+        VBox btnBox = new VBox();
+        cardBox.setPadding(new Insets(5));
+        btnBox.setPadding(new Insets(25));
 
-        button.setOnAction(new EventHandler<ActionEvent>() {
+        FlowPane rootPane = new FlowPane();
+        rootPane.setPadding(new Insets(25));
+
+        FlowPane infoPane = new FlowPane();
+        infoPane.setPadding(new Insets(25));
+
+        Button dealBtn = new Button();
+        dealBtn.setText("Deal hand");
+
+        Button checkBtn = new Button();
+        checkBtn.setText("Check hand");
+
+        infoPane.getChildren().addAll(createInfoField("Sum of the faces: "),
+                createInfoField("Cards of hearts: "),
+                createInfoField("Flush: "),
+                createInfoField("Queen of spades: "));
+
+        final ArrayList<PlayingCard> hand = new ArrayList<>();
+
+        dealBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                cardBox.getChildren().clear();
+                hand.clear();
+
+                DeckOfCards deck = new DeckOfCards();
+                hand.addAll(deck.dealHand(5));
+
+
+                ArrayList<StackPane> cardImages = new ArrayList<>();
+                for (PlayingCard card : hand) {
+                    cardImages.add(card.getCardImage());
+                }
+
+                cardBox.getChildren().addAll(cardImages);
             }
         });
 
-        HBox rootBox = new HBox();
-        //TODO this shouldnt be stack pane because they will stack on each other
-        //rootPane.getChildren().add(button);
+        checkBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if (hand.isEmpty()) {
+                    return;
+                }
 
+                showSumOfFaces(hand, infoPane);
+                showCardsOfHearts(hand, infoPane);
+                showFlush(hand, infoPane);
+                showQueenOfSpades(hand, infoPane);
+            }
+        });
 
-        ArrayList<PlayingCard> hand;
-        DeckOfCards deck = new DeckOfCards();
-        hand = deck.dealHand(5);
+        rootPane.getChildren().add(cardBox);
 
+        btnBox.getChildren().add(dealBtn);
+        btnBox.getChildren().add(checkBtn);
+        rootPane.getChildren().add(btnBox);
 
-        ArrayList<StackPane> cardImages = new ArrayList<>();
-        for (PlayingCard card : hand) {
-            cardImages.add(card.getCardImage());
-        }
+        rootPane.getChildren().add(infoPane);
 
-        rootBox.getChildren().addAll(cardImages);
-
-        Scene scene = new Scene(rootBox, 1280, 720);
+        Scene scene = new Scene(rootPane, 980, 620);
+        primaryStage.setTitle("Card Game");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
